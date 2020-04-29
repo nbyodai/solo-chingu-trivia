1c7691e883b392d520ecc0da81d30af7e93c5938
Chibuzor Obiora <nbyodai@gmail.com>
2020-04-29 20:16:56 +0100

    Add placeholder entry component and file

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app" class="container mx-auto">
+    <h1 class="text-6xl font-bold text-green-600">Chingu Chingu</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  components: {},
+};
+</script>
