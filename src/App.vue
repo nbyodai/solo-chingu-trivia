dde2497155d1cfb3fbfdee715e67928c0f5d8338
Chibuzor Obiora <nbyodai@gmail.com>
2020-05-03 17:47:47 +0100

    Display button to move to next question

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
   <div id="app" class="container h-screen mx-auto">
     <home-header />
-    <div class="flex flex-col mt-16 border-2 border-gray-100 shadow-lg">
+    <div class="flex flex-col items-center mt-16 pb-4 border-2 border-gray-100 shadow-lg">
       <question-tracker :total="total" :step="step" />
-      <question-card :question="questions[0]" />
+      <question-card :question="questions[step-1]" @answered="answered" />
+      <div>
+        <button
+          class="bg-blue-600 px-3 py-3 w-20 rounded"
+          @click="goToNext"
+          v-show="stepAnswered"
+        >Next</button>
+      </div>
     </div>
   </div>
 </template>
@@ -25,7 +32,7 @@ export default {
       questions: [
         {
           question:
-            "Which HTML5 element should contain important information about what page you are on and the topic of the page?",
+            "Which HTML5 element should contain important links for navigating a website?",
           id: 1,
           topic: "html",
           choices: {
@@ -35,19 +42,52 @@ export default {
             d: "<nav>"
           },
           answer: "d"
+        },
+        {
+          question:
+            "Which HTML5 element should contain important information about what page you are on and the topic of the page?",
+          id: 2,
+          topic: "html",
+          choices: {
+            a: "<li>",
+            b: "<ul>",
+            c: "<header>",
+            d: "<nav>"
+          },
+          answer: "c"
         }
-      ]
+      ],
+      step: 1,
+      stepAnswered: false,
+      stepAnswer: null
     };
   },
   computed: {
     total: function() {
       return this.questions.length;
+    }
+  },
+  methods: {
+    goToNext: function() {
+      const isNotLastQuestion = this.step < this.questions.length;
+      if (isNotLastQuestion) {
+        this.resetStepValues();
+        this.step++;
+      } else {
+        return;
+      }
     },
-    step: function() {
-      return 1;
+    answered: function(answer) {
+      this.stepAnswered = true;
+      this.stepAnswer = answer;
+    },
+    resetStepValues: function() {
+      this.stepAnswered = false;
+      this.stepAnswer = null;
     }
   }
 };
 </script>
 
 <style src="./assets/css/style.css"></style>
+
--- src/components/QuestionCard.vue
+++ src/components/QuestionCard.vue
@@ -2,8 +2,15 @@
   <div class="flex flex-col px-5 items-center">
     <div class="w-4/5 text-center">{{ question.question }}</div>
     <div class="flex flex-wrap justify-center w-4/5 py-4">
-      <div class="w-2/5 p-2 mx-2 my-2 bg-gray-100 shadow-md" v-for="choice in question.choices" :key="choice">
-        <div class="p-2 flex items-center justify-center text-sm font-semibold text-gray-700">{{choice}}</div>
+      <div
+        class="w-2/5 p-2 mx-2 my-2 bg-gray-100 shadow-md"
+        v-for="[key, choice] of Object.entries(question.choices)"
+        :key="key"
+      >
+        <div
+          class="p-2 flex items-center justify-center text-sm font-semibold text-gray-700 cursor-pointer"
+          @click="$emit('answered', key)"
+        >{{choice}}</div>
       </div>
     </div>
   </div>
