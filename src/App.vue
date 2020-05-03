e344e39ce719f178a27aa90b5496c83b140df275
Chibuzor Obiora <nbyodai@gmail.com>
2020-05-03 16:05:40 +0100

    Add card that displays trivia question and choices

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,40 @@
 <template>
-  <div id="app" class="container mx-auto">
+  <div id="app" class="container h-screen mx-auto">
     <home-header />
+    <div class="flex flex-col mt-16">
+      <question-card :question="questions[0]" />
+    </div>
   </div>
 </template>
 
 <script>
 import HomeHeader from "./components/Header.vue";
+import QuestionCard from "./components/QuestionCard.vue";
 export default {
   name: "app",
   components: {
-      HomeHeader,
+    HomeHeader,
+    QuestionCard
   },
+  data: function() {
+    return {
+      questions: [
+        {
+          question:
+            "Which HTML5 element should contain important information about what page you are on and the topic of the page?",
+          id: 1,
+          topic: "html",
+          choices: {
+            a: "<li>",
+            b: "<ul>",
+            c: "<header>",
+            d: "<nav>"
+          },
+          answer: "d"
+        }
+      ]
+    };
+  }
 };
 </script>
 
---
+++ src/components/QuestionCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col px-5 items-center border-2 border-gray-100 shadow-lg">
+    <div class="w-4/5 text-center">{{ question.question }}</div>
+    <div class="flex flex-wrap justify-center w-4/5 py-4">
+      <div class="w-2/5 p-2 mx-2 mb-2 bg-gray-100 shadow-md" v-for="choice in question.choices" :key="choice">
+        <div class="p-2 flex items-center justify-center text-sm font-semibold text-gray-700">{{choice}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    question: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
