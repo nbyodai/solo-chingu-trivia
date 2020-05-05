212572fb83a7ff42afa359097d21389bd749be46
Chibuzor Obiora <nbyodai@gmail.com>
2020-05-05 17:44:59 +0100

    Disable buttons on question answer

--- src/components/QuestionCard.vue
+++ src/components/QuestionCard.vue
@@ -2,16 +2,18 @@
   <div class="flex flex-col px-5 items-center">
     <div class="w-4/5 text-center">{{ question.question }}</div>
     <div class="flex flex-wrap justify-center w-4/5 py-4">
-      <div
-        class="w-2/5 p-2 mx-2 my-2 bg-gray-100 shadow-md"
+      <button
+        class="w-2/5 p-2 mx-2 my-2 bg-gray-100 shadow-md cursor-pointer"
+        :class="{ 'cursor-pointer': !disabled, 'cursor-not-allowed': disabled }"
         v-for="[key, choice] of Object.entries(question.choices)"
         :key="key"
+        :disabled="disabled"
+        @click="emit(key)"
       >
         <div
-          class="p-2 flex items-center justify-center text-sm font-semibold text-gray-700 cursor-pointer"
-          @click="$emit('answered', key)"
+          class="p-2 flex items-center justify-center text-sm font-semibold text-gray-700"
         >{{choice}}</div>
-      </div>
+      </button>
     </div>
   </div>
 </template>
@@ -22,6 +24,17 @@ export default {
     question: {
       type: Object,
       required: true
+    }
+  },
+  data: function() {
+    return {
+      disabled: false
+    };
+  },
+  methods: {
+    emit: function(key) {
+      this.disabled = true;
+      this.$emit("answered", key);
     }
   }
 };
