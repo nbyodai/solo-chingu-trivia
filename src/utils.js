f37a030b96d40535a0aeed7924113e3154fe72da
Chibuzor Obiora <nbyodai@gmail.com>
2020-05-06 06:56:31 +0100

    Refactor utility package

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,10 @@
 import { generate } from "rand-token";
 
+const USER_TOKEN = "USER_TOKEN";
+
 export const generateToken = () => generate(16);
 
-export const getLocalStorageUser = () => localStorage.getItem("USER_TOKEN");
+export const getLocalStorageUser = () => localStorage.getItem(USER_TOKEN);
 
 export const setLocalStorageUser = (token) =>
-  localStorage.setItem("USER_TOKEN", token);
+  localStorage.setItem(USER_TOKEN, token);
